4eeb0ab251c690eda2bbf74febc69bdeb4b3ff31
JuanLP <juan.luis.palacios.p@gmail.com>
2022-07-11 22:30:59 -0500

    Abstract Layer componet into Container

--- src/componets/Layer.tsx
+++ src/componets/Layer.tsx
@@ -1,54 +1,13 @@
-import app,{ Component, VNode } from 'apprun';
-import { InteractiveLayerOptions, Layer as LeafletLayer, LayerOptions } from 'leaflet';
+import { Layer as LeafletLayer} from 'leaflet';
 import { ContextBased } from '../types/ContextBased';
-import { EventedProps } from '../types/EventedProps';
+import { Container, ContainerProps } from '../abstracts/Container';
 
-export interface LayerProps extends EventedProps, LayerOptions {
-    children?: Array<VNode | string>
-}
-export interface InteractiveLayerProps
-  extends LayerProps,
-    InteractiveLayerOptions {}
-
-export class Layer<T extends LeafletLayer, E extends LayerProps> extends Component<E> {
-  layer?:T;
-  state!:ContextBased<E>;
-
-  constructor(props:ContextBased<E>,...p){
-    super(props,...p);
+export class Layer extends Container<LeafletLayer, ContainerProps> {
+  createLayer(props: ContextBased<ContainerProps>): LeafletLayer {
+    return new LeafletLayer(props);
   }
 
-  view = (state = this.state) => {
-    if (!this.layer) {
-      this.layer = this.createLayer(state);
-    }
-    if(state.context) {
-      this.layer.addTo(state.context);
-    }
-    return <div>
-      {state.children}
-    </div>;
-  };
-
-  mounted = (props: ContextBased<E>, children: any[], state: ContextBased<E>) => {
-    const context = this.layer;
-    state.children = state.children?.map(node=>
-      typeof node === 'string'?
-        node :(
-          (node.tag as any).prototype instanceof Component? { ...node, props:{ ...node.props ,context } } : node
-        )
-    );
-    if (props.context !== state.context) {
-      if(state.context) {this.layer.remove();}
-      this.layer.addTo(props.context);
-    }
-    this.updateLayer(this.layer, props, state);
-    return { ...props, children };
-  };
-
-  createLayer = (props:ContextBased<E>)=>{
-    return (new LeafletLayer(props) as T);
-  };
-
-  updateLayer = (layer:T, props:ContextBased<E>, prevProps:ContextBased<E>)=>{};
+  updateLayer(layer: LeafletLayer, props: ContextBased<ContainerProps>, prevProps: ContextBased<ContainerProps>): void {
+    
+  }
 }
--- src/componets/TileLayer.tsx
+++ src/componets/TileLayer.tsx
@@ -1,18 +1,18 @@
 import { TileLayer as LeafletTileLayer, TileLayerOptions } from 'leaflet';
 import { ContextBased } from '../types/ContextBased';
-import { Layer, LayerProps } from './Layer';
+import { Container, ContainerProps } from '../abstracts/Container';
 
-export interface TileLayerProps extends TileLayerOptions, LayerProps {
+export interface TileLayerProps extends TileLayerOptions, ContainerProps {
     url: string
   }
 
-export class TileLayer extends Layer<LeafletTileLayer, TileLayerProps> {
-  createLayer = (props: ContextBased<TileLayerProps>) => {
+export class TileLayer extends Container<LeafletTileLayer, TileLayerProps> {
+  createLayer(props: ContextBased<TileLayerProps>): LeafletTileLayer {
     const { url, ...options } = props;
     return new LeafletTileLayer(url, options);
   };
 
-  updateLayer = (layer:LeafletTileLayer, props:ContextBased<TileLayerProps>, prevProps:ContextBased<TileLayerProps>)=>{
+  updateLayer(layer: LeafletTileLayer, props: ContextBased<TileLayerProps>, prevProps: ContextBased<TileLayerProps>): void {
     if (props.url != null && props.url !== prevProps.url) {
       layer.setUrl(props.url);
     }
